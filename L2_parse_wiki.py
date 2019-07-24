33a40ed290f4643ca1f58022fe4483a763dda8fe
hildar <shousediy@gmail.com>
2019-07-24 19:06:39 +0900

    HW. Execute Task 2, 3

--- L2_parse_wiki.py
+++ L2_parse_wiki.py
@@ -1,21 +1,87 @@
+# Задание 2. В приложении парсинга википедии получить первую ссылку на другую страницу и вывести все
+# значимые слова из неё. Результат записать в файл в форматированном виде
+#
+# Задание 3. *Научить приложение определять количество ссылок в статье.
+# Спарсить каждую ссылку и результаты
+# записать в отдельные файлы.
+
+
 import collections
-import requests
 import re
+import requests
+import csv
 
 
-def return_wiki_html(topic):
-    wiki_request = requests.get(f'https://ru.wikipedia.org/wiki/{topic.capitalize()}')
-    return wiki_request.text
+# Return HTML from site
+def return_site_html(link):
+    try:
+        site_request = requests.get(link)
+    except ConnectionError:
+        print('Connection error to site: ru.wikipedia.org')
+        exit(1)
+    return site_request.text
 
-def return_words(topic):
-    wiki_html = return_wiki_html(topic)
-    words = re.findall('[а-яА-Я]{3,}', wiki_html)
+
+# Return links from site
+def return_links(url):
+    site_html = return_site_html(url)
+    links = re.findall('<li><a\s.*?href=\"([hf].+?)\".*?>', site_html)
+    # Correct links if there are ampersand character - &
+    for i in range(len(links)):
+        links[i] = links[i].replace('amp;', '')
+    return links
+
+
+# Return words from site
+def return_words_from_site(link):
+    site_html = return_site_html(link)
+    words = re.findall('[а-яА-Я]{3,}', site_html)
+    # Count words
     words_counter = collections.Counter()
     for word in words:
         words_counter[word] += 1
-    for word in words_counter.most_common(10):
-        print(f'Слово {word[0]} встречается {word[1]} раз')
     return words_counter.most_common(10)
 
 
-print(return_words('Трям!_Здравствуйте!'))
+# Get names from links
+def get_names(links):
+    links = str(links)
+    spam = re.findall('\'https?://(www.)?([\w\.-]+)', links)
+    names = []
+    # Get only domain without 'www'
+    for egg in spam:
+        names.append(egg[1])
+    # Replace dots and dashes in names
+    for i in range(len(names)):
+        names[i] = names[i].replace('.', '_')
+        names[i] = names[i].replace('-', '_')
+    # Add '2' if double names
+    for i in range(len(names) - 1):
+        for j in range(i + 1, len(names)):
+            if names[j] == names[i]:
+                names[j] = names[j] + '2'
+    return names
+
+
+# Write words to files
+def file_writers(file_name, words):
+    with open('parsing.git/L2_files/' + file_name + '.csv', 'w') as file:
+        pen = csv.writer(file)
+        pen.writerow(('Слово', 'Количество вхождений'))
+        for word in words:
+            pen.writerow((word[0], word[1]))
+
+
+# Return words, links and write to files
+def main_parsing(url):
+    wiki_links = return_links(url)
+    file_names = get_names(wiki_links)
+    # Write to files
+    for i, link in enumerate(wiki_links):
+        spam = return_words_from_site(link)
+        file_writers(file_names[i], spam)
+    return f'Слова с {len(file_names)} сайтов записаны в файлы по адресу "parsing.git/L2_files/"'
+
+
+main_url = 'https://ru.wikipedia.org/wiki/Трям!_Здравствуйте!'
+print(main_parsing(main_url))
