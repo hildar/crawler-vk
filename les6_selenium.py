38ff308c9cd1c2913f4327fe1010b7547cd37ec3
hildar <shousediy@gmail.com>
2019-08-07 19:54:25 +0900

    add navigate back

--- les6_selenium.py
+++ les6_selenium.py
@@ -4,7 +4,7 @@
 данных (от кого, дата отправки, тема письма, текст письма)
 """
 
-import time
+from time import sleep
 from selenium import webdriver  # Основной элемент
 from selenium.webdriver.common.keys import Keys  # Клавиши клавиатуры
 from pymongo import MongoClient
@@ -18,14 +18,14 @@ def parse_site_with_selenium():
     elem = driver.find_element_by_id('mailbox:password')
     elem.send_keys('youpieceofmeat')
     elem.send_keys(Keys.RETURN)
-    time.sleep(8)  # Download page maybe so long
+    sleep(8)  # Download page maybe so long
     letters_class_name = 'llc js-tooltip-direction_letter-bottom js-letter-list-item llc_normal'
     len_letters = len(driver.find_elements_by_xpath(f'//a[contains(@class, "{letters_class_name}")]'))
     letters_all = []
     for i in range(len_letters):
         letters = driver.find_elements_by_xpath(f'//a[contains(@class, "{letters_class_name}")]')
         letters[i].click()
-        time.sleep(3)
+        sleep(3)
         letter_author = driver.find_element_by_class_name('letter__contact-item').text
         letter_date = driver.find_element_by_class_name('letter__date').text
         letter_topic = driver.find_element_by_class_name('thread__subject').text
@@ -34,28 +34,46 @@ def parse_site_with_selenium():
                             'date': letter_date,
                             'topic': letter_topic,
                             'content': letter_content})
-        driver.get('https://e.mail.ru/inbox/')
-        time.sleep(3)
+        driver.back()
+        sleep(3)
     driver.quit()
     return letters_all
 
+#
+# def client_mongo():
+#     client = MongoClient('mongodb://127.0.0.1:27017')
+#     data_base = client['db_mail_letters']  # db name
+#     mail_letters = data_base.mail_letters  # collection name
+#     return mail_letters
+#
+#
+# def save_to_mongo(letters: list):
+#     mail_letters = client_mongo()
+#     count = 0
+#     for letter in letters:
+#         spam = mail_letters.find_one({'content': letter['content']})
+#         if spam is None:
+#             mail_letters.insert_one(letter)
+#             count += 1
+#     print(f'Added {count} records. Collection "{mail_letters.name}" has {mail_letters.count_documents({})} letters')
 
-def client_mongo():
+
+def client_mongo(db_name: str, collection_name: str):
     client = MongoClient('mongodb://127.0.0.1:27017')
-    data_base = client['db_mail_letters']  # db name
-    mail_letters = data_base.mail_letters  # collection name
-    return mail_letters
+    data_base = client[db_name]  # db name
+    collect_name = data_base[collection_name] # collection name
+    return collect_name
 
 
-def save_to_mongo(letters: list):
-    mail_letters = client_mongo()
+def save_to_mongo(list_to_save_db: list, unique_key: str, db_name: str, collection_name: str):
+    collection = client_mongo(db_name, collection_name)
     count = 0
-    for letter in letters:
-        spam = mail_letters.find_one({'content': letter['content']})
+    for item in list_to_save_db:
+        spam = collection.find_one({unique_key: item[unique_key]})
         if spam is None:
-            mail_letters.insert_one(letter)
+            collection.insert_one(item)
             count += 1
-    print(f'Added {count} records. Collection "{mail_letters.name}" has {mail_letters.count_documents({})} letters')
+    print(f'Added {count} records. Collection "{collection.name}" has {collection.count_documents({})} items.')
 
 
-save_to_mongo(parse_site_with_selenium())
+save_to_mongo(parse_site_with_selenium(), 'content', 'db_mail_letters', 'mail_letters')
